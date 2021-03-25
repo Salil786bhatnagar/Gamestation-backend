d207f57dde02de193150da56be7a334ce420102b
salil bhatnagar <salilbhatnagar1@gmail.com>
2021-03-25 19:04:20 +0530

    gamtation bakend

---
+++ routes/api/multer.js
@@ -0,0 +1,13 @@
+const shortid = require('shortid');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, 'public/images')
+    },
+    filename: (req, file, cb)=>{
+        cb(null,file.originalname)
+    },
+});
+
+module.exports = multer({storage});;
